80eb668b6da675f8edc3f401f9cdc9065c2efe03
duyquang <duyquangnvx@gmail.com>
2022-07-02 01:15:33 +0700

    update smart contract

--- smart_contract/contracts/LicenseToken.sol
+++ smart_contract/contracts/LicenseToken.sol
@@ -1,5 +1,5 @@
- pragma solidity 0.8.15;
- 
+ pragma solidity 0.8.7;
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
@@ -21,10 +21,14 @@ contract LicenseToken is Ownable, ERC721 {
     LicenseState state;
     string deviceId;
   }
-   
+
+  mapping(address => LicenseInfo[]) tokensOfAccount;
+  mapping(address => uint256[]) tokenIdsOfAccount;
+
   LicenseInfo[] tokens;
   event LicenseGiven(address account, uint256 tokenId);
-   
+  event LicenseActivate(address account, uint256 tokenId, uint state, uint expiresOn, string deviceId); 
+
   constructor(string memory name, string memory symbol) ERC721(name, symbol)  {}
    
    function transferFrom(address from, address to, uint256 tokenId) onlyOwner public virtual override {
@@ -36,11 +40,13 @@ contract LicenseToken is Ownable, ERC721 {
     uint256 tokenId = _tokenIds.current();
     _tokenIds.increment();
     _mint(_account, tokenId);
-    _createLicense(_type);
+    LicenseInfo memory token = _createLicense(_type);
+    tokensOfAccount[_account].push(token);
+    tokenIdsOfAccount[_account].push(tokenId);
     emit LicenseGiven(_account, tokenId);
   }
 
-  function _createLicense(uint256 _type) onlyOwner internal {
+  function _createLicense(uint256 _type) onlyOwner internal returns (LicenseInfo memory) {
     LicenseInfo memory token = LicenseInfo({
         licenseType: LicenseType(_type),
         state: LicenseState.INACTIVE,
@@ -49,6 +55,20 @@ contract LicenseToken is Ownable, ERC721 {
         deviceId: ""
     });
     tokens.push(token);
+    return token;
+  }
+
+  function getAllTokensOfAccount(address _account) onlyOwner public view returns (LicenseInfo[] memory) {
+      return tokensOfAccount[_account];
+  }
+  function getAllTokenIdsOfAccount(address _account) onlyOwner public view returns (uint256[] memory) {
+      return tokenIdsOfAccount[_account];
+  }
+
+  function getTokenInfo(address _account, uint256 _tokenId) onlyOwner public view returns (LicenseInfo memory) {
+    require(_exists(_tokenId), "tokenId is not available!");
+    require(ownerOf(_tokenId) == _account);
+    return tokens[_tokenId];
   }
 
   function _getTokenByTokenId(uint256 _tokenId) onlyOwner internal view returns (LicenseInfo storage) {
@@ -67,6 +87,8 @@ contract LicenseToken is Ownable, ERC721 {
     token.state = LicenseState.ACTIVE;
     token.expiresOn = block.timestamp + LICENSE_LIFE_TIME;
     token.deviceId = _deviceId;
+    
+    emit LicenseActivate(_account, _tokenId, uint(token.state), token.expiresOn, token.deviceId);
   }
    
   function isLicenseActive(address _account, uint256 _tokenId) public view returns (uint state){
@@ -89,6 +111,17 @@ contract LicenseToken is Ownable, ERC721 {
     if (token.expiresOn < block.timestamp && token.state == LicenseState.ACTIVE) {
        _burn(_tokenId);
        delete tokens[_tokenId];
+       removeTokenInTokensOfAccount(_account, _tokenId);
+    }
+  }
+
+  function removeTokenInTokensOfAccount(address _account, uint256 _tokenId) onlyOwner internal {
+    for (uint256 i = 0; i < tokenIdsOfAccount[_account].length; i++) {
+      if (tokenIdsOfAccount[_account][i] == _tokenId) {
+        delete tokensOfAccount[_account][i];
+        delete tokenIdsOfAccount[_account][i];
+        break;
+      }
     }
   }
    
@@ -96,4 +129,5 @@ contract LicenseToken is Ownable, ERC721 {
   function _owns(address _claimant, uint256 _tokenId) internal view returns (bool) {
     return ownerOf(_tokenId) == _claimant;
   }
+
 }

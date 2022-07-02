a9135ce8b7f7134d048c53580ff6916089077071
duyquang <duyquangnvx@gmail.com>
2022-07-03 01:57:07 +0700

    update web3 interact smart contract in ropsten network

--- client/src/index.js
+++ client/src/index.js
@@ -8,13 +8,16 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 const LicenseConfig = require('./license_token/license-config');
 const LicenseToken = require('./license_token/license-token');
+
 (async () => {
     await LicenseToken.initContract();
-    const address = '0x25DeDdc3B3de5fd78F808F871C1A4Cbb8A28315b';   
+    const address = '0x94301fD24a45855996ED88Fb9460B77A30284DE2';   
     const result = await LicenseToken.send(LicenseToken.METHOD_GIVE_LICENSE, address, LicenseConfig.LICENSE_TYPE_WINDOWS);
-    console.log('jkkkkkkkkkkkk', result);
+    const tokenId = result.events.LicenseGiven.returnValues.tokenId;
+    const registeredOn = result.events.LicenseGiven.returnValues.registeredOn;
+    console.log('----------------', result);
 
-    // const address = '0x94301fD24a45855996ED88Fb9460B77A30284DE2';   
+    // const address = '0x25DeDdc3B3de5fd78F808F871C1A4Cbb8A28315b';   
     // const tokensOfAccount = await LicenseToken.call(LicenseToken.METHOD_GET_TOKENS_OF_ACCOUNT, address);
     // const tokenIdsOfAccount = await LicenseToken.call(LicenseToken.METHOD_GET_TOKENS_ID_OF_ACCOUNT, address);
 

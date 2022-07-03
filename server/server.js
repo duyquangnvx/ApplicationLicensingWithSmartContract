9c0a18d810e07d8ad229dcd283681bd7da3031fd
duyquang <duyquangnvx@gmail.com>
2022-07-03 14:35:47 +0700

    add cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express'); 
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json({extended: false}));
